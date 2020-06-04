24ad0f401d50e6d9891f124608638313942897b9
banisholih23 <banisholih23@gmail.com>
2020-06-04 13:12:58 +0700

    library-react

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, {Component} from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
-// import Login from './pages/Login'
-// import Register from './pages/Register'
+import Login from './pages/Login'
+import Register from './pages/Register'
 import Details from './pages/Details'
 
 // class App extends Component{
@@ -14,26 +15,22 @@ import Details from './pages/Details'
 //   }
 // }
 
-// class Regist extends Component{
-//   render(){
-//     return(
-//       <>
-//         <Register />
-//       </>
-//     )
-//   }
-// }
+function App() {
+  return (
+    <>
+    
+      <BrowserRouter>
+        <Switch>
+          <Route path="/login" exact component={Login}/>
+          <Route path="/register" exact component={Register}/>
+          <Route path="/details" exact component={Details}/>
+        </Switch>
+      </BrowserRouter>
 
-class Detail extends Component{
-  render(){
-    return(
-      <>
-        <Details />
-      </>
-    )
-  }
+    </>
+  );
 }
 
-// export default App
+export default App
 // export default Regist
-export default Detail
+// export default Detail
